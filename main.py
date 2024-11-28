38e1e365c90a0e4285a0c8f2557ad85fbffe0998
KovacsTL <kovacstl03@gmail.com>
2024-11-28 08:49:16 +0100

    Beadandó

--- main.py
+++ main.py
@@ -8,24 +8,24 @@ class GUI_KTL:
         self.root.geometry("500x350")
         self.root.resizable(False, False)
 
-        self.szo_titkosito_KTL = SzovegTitkositoKTL()
+        self.szo_titkosito = SzovegTitkositoKTL()
 
         #Szöveg bemenet és kimenetek
-        self.szoveg_bemenet_KTL = Entry(root, width=50)
-        self.titkositott_kimenet_KTL = StringVar()
-        self.visszafejtett_kimenet_KTL = StringVar()
+        self.szoveg_bemenet = Entry(root, width=50)
+        self.titkositott_kimenet = StringVar()
+        self.visszafejtett_kimenet = StringVar()
 
         self.ui_KTL()
 
     def ui_KTL(self):
         Label(self.root, text="Titkosítandó szöveg:").pack(pady=5)
-        self.szoveg_bemenet_KTL.pack()
+        self.szoveg_bemenet.pack()
 
         Label(self.root, text="Titkosított szöveg:").pack(pady=5)
-        Entry(self.root, textvariable=self.titkositott_kimenet_KTL, width=50).pack()
+        Entry(self.root, textvariable=self.titkositott_kimenet, width=50).pack()
 
         Label(self.root, text="Visszafejtett szöveg:").pack(pady=5)
-        Entry(self.root, textvariable=self.visszafejtett_kimenet_KTL, width=50).pack()
+        Entry(self.root, textvariable=self.visszafejtett_kimenet, width=50).pack()
 
         Button(self.root, text="Titkosítás", command=self.titkositas_KTL).pack(pady=10)
         Button(self.root, text="Visszafejtés", command=self.visszafejtes_KTL).pack(pady=5)
@@ -34,26 +34,26 @@ class GUI_KTL:
         Button(self.root, text="Kilépés", command=self.root.quit).pack(pady=50)
 
     def titkositas_KTL(self):
-        szoveg_KTL = self.szoveg_bemenet_KTL.get()
-        titkositott_KTL = self.szo_titkosito_KTL.titkositas_KTL(szoveg_KTL)
-        self.titkositott_kimenet_KTL.set(titkositott_KTL)
+        szoveg = self.szoveg_bemenet.get()
+        titkositott = self.szo_titkosito.titkositas_KTL(szoveg)
+        self.titkositott_kimenet.set(titkositott)
 
     def visszafejtes_KTL(self):
-        titkositott_KTL = self.titkositott_kimenet_KTL.get()
-        visszafejtett_KTL = self.szo_titkosito_KTL.visszafejtes_KTL(titkositott_KTL)
-        self.visszafejtett_kimenet_KTL.set(visszafejtett_KTL)
+        titkositott = self.titkositott_kimenet.get()
+        visszafejtett = self.szo_titkosito.visszafejtes_KTL(titkositott)
+        self.visszafejtett_kimenet.set(visszafejtett)
 
     def kulcs_mentes_KTL(self):
-        fajlnev_KTL = "kulcs_KTL.txt"
-        self.szo_titkosito_KTL.kulcs_mentes_txt_KTL(fajlnev_KTL)
-        print(f"Kulcs mentve: {fajlnev_KTL}")
+        file = "kulcs.txt"
+        self.szo_titkosito.kulcs_mentes_txt_KTL(file)
+        print(f"Kulcs mentve: {file}")
 
     def kulcs_betolt_KTL(self):
-        fajlnev_KTL = "kulcs_KTL.txt"
-        self.szo_titkosito_KTL.kulcs_betoltes_txt_KTL(fajlnev_KTL)
-        print(f"Kulcs betöltve: {fajlnev_KTL}")
+        file = "kulcs.txt"
+        self.szo_titkosito.kulcs_betoltes_txt_KTL(file)
+        print(f"Kulcs betöltve: {file}")
 
 if __name__ == "__main__":
-    root_KTL = Tk()
-    app_KTL = GUI_KTL(root_KTL)
-    root_KTL.mainloop()
+    root = Tk()
+    app = GUI_KTL(root)
+    root.mainloop()
--- szoveg_titkosito_KTL.py
+++ szoveg_titkosito_KTL.py
@@ -3,39 +3,39 @@ import random
 #Karakterek listája és kulcs generálása
 class SzovegTitkositoKTL:
     def __init__(self):
-        self.karakterek_KTL = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÚÜÖÓŰabcdefghijklmnopqrstuvwxyzáéúüöóű!%.?_-@*(){}'=/,;: "
-        self.karakterek_KTL = list(self.karakterek_KTL)
-        self.kulcs_KTL = self.karakterek_KTL.copy()
-        random.shuffle(self.kulcs_KTL)
+        self.karakterek = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZÁÉÚÜÖÓŰabcdefghijklmnopqrstuvwxyzáéúüöóű!%.?_-@*(){}'=/,;: "
+        self.karakterek = list(self.karakterek)
+        self.kulcs = self.karakterek.copy()
+        random.shuffle(self.kulcs)
 
     #Szöveg titkosítása
-    def titkositas_KTL(self, szoveg_KTL):
-        titkositott_KTL = ""
-        for betu_KTL in szoveg_KTL:
-            if betu_KTL in self.karakterek_KTL:
-                index_KTL = self.karakterek_KTL.index(betu_KTL)
-                titkositott_KTL += self.kulcs_KTL[index_KTL]
+    def titkositas_KTL(self, szoveg):
+        titkositott = ""
+        for betu in szoveg:
+            if betu in self.karakterek:
+                index = self.karakterek.index(betu)
+                titkositott += self.kulcs[index]
             else:
-                titkositott_KTL += betu_KTL
-        return titkositott_KTL
+                titkositott += betu
+        return titkositott
 
     #Titkosított szöveg visszafejtése
-    def visszafejtes_KTL(self, titkositott_KTL):
-        visszafejtett_KTL = ""
-        for betu_KTL in titkositott_KTL:
-            if betu_KTL in self.kulcs_KTL:
-                index_KTL = self.kulcs_KTL.index(betu_KTL)
-                visszafejtett_KTL += self.karakterek_KTL[index_KTL]
+    def visszafejtes_KTL(self, titkositott):
+        visszafejtett = ""
+        for betu in titkositott:
+            if betu in self.kulcs:
+                index = self.kulcs.index(betu)
+                visszafejtett += self.karakterek[index]
             else:
-                visszafejtett_KTL += betu_KTL
-        return visszafejtett_KTL
+                visszafejtett += betu
+        return visszafejtett
 
     #Kulcs mentése
     def kulcs_mentes_txt_KTL(self, file):
         with open(file, 'w') as file:
-            file.write(' '.join(self.kulcs_KTL))
+            file.write(' '.join(self.kulcs))
 
     #Kulcs betöltése
     def kulcs_betoltes_txt_KTL(self, file):
         with open(file, 'r') as file:
-            self.kulcs_KTL = file.read().split()
+            self.kulcs = file.read().split()
